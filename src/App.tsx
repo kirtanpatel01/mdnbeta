b1d3249548999d5358e29ea467ecb5fa6a9b861b
Kirtan Patel <kjpatel200022@gmail.com>
2026-05-11 14:20:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Header from './components/header'
+import Hero from './components/hero'
+import Companies from './components/companies'
+import Features from './components/features'
+import Product from './components/product'
+import FAQs from './components/faqs'
+import Footer from './components/footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Companies />
+      <Features />
+      <Product />
+      <FAQs />
+      <Footer />
+    </>
+  )
+}
+
+export default App
